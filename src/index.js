4652f4dce8d81cd8f61b7ba8862bba83c6fa2685
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-19 17:29:26 -0500

    commit tree 64ec7cbc311363f8cf8aca4319e8ae6c7834619d

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import counterReducer from './reducers/counterReducer.js';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux"; /* We imported Provider from React Redux / We used Provider to wrap our React application*/
+import counterReducer from "./reducers/counterReducer.js";
+import App from "./App";
+import "./index.css";
 
 const store = createStore(
-    counterReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ); 
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider> /* code change */,
+  document.getElementById("root")
+);
